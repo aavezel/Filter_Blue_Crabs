c9e52324a9c0fa304945dd3e6bd816354e65558e
aavezel <aavezel@sprintinet.ru>
2018-11-14 14:57:36 +0300

    Code refactoring. Modify extract.js to the ECMAScript 2017 format

--- src/extract.js
+++ src/extract.js
@@ -6,21 +6,58 @@ var config = require("../config");
 
 main();
 
-function main() {
-    if (!fs.existsSync(config.types_file)) {
-        // eslint-disable-next-line quotes
-        fs.writeFile(config.types_file, `"channel","type"\n`, (error) => {
-            if (error) console.error(error);
-        });
-    }
-    config.m3u.forEach((file) => fs.readFile(file, "utf8", parseM3U));
+async function main() {
+    await createTypesFileIfNeeded();
+    config.m3u.forEach(async (file) => {
+        try {
+            const data = await loadFile(file);
+            const table = parseM3U(data);
+            const csv_string = await stringifyCSV(table);
+            await writeToFile(csv_string);
+        } catch (e) {
+            console.error(`Не удалось распарсить файл ${file}. Ошибка ${e}`);
+        }
+    });
+    console.log("All ok");
 }
 
-function parseM3U(err, data) {
-    if (err) {
-        console.error(err);
-        return;
+async function createTypesFileIfNeeded() {
+    const exist = await checkExistsTypesFile();
+    if (!exist) {
+        await writeHeader();
     }
+}
+
+async function checkExistsTypesFile() {
+    return new Promise((res) => {
+        fs.exists(config.types_file, (exists) => {
+            res(exists);
+        });
+    });
+}
+
+async function writeHeader() {
+    return new Promise((res, rej) => {
+        // eslint-disable-next-line quotes
+        fs.writeFile(config.types_file, `"channel","type"\n`, (error) => {
+            if (error) rej(error);
+            else res();
+        });
+    });
+}
+
+async function loadFile(file_name) {
+    return new Promise((res, rej) => {
+        fs.readFile(file_name, "utf8", (error, data) => {
+            if (error) rej(error);
+            else res(data);
+        });
+    });
+}
+
+
+
+function parseM3U(data) {
     const playlist_str_split = data.split("\n").filter(f => f.startsWith("#EXTINF"));
 
     let result = [];
@@ -32,20 +69,31 @@ function parseM3U(err, data) {
         }
     }
 
-    csv.stringify(result, {
-        columns: ["channel", "type"],
-        delimiter: ",",
-        header: false,
-        quoted: true
-    }, writeData2File);
+    return result;
 }
 
-function writeData2File(err, data) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    fs.appendFile(config.types_file, data, (error) => {
-        if (error) throw error;
+async function stringifyCSV(table) {
+    return new Promise((res, rej) => {
+        csv.stringify(
+            table, {
+                columns: ["channel", "type"],
+                delimiter: ",",
+                header: false,
+                quoted: true
+            },
+            (err, table) => {
+                if (err) rej(err);
+                else res(table);
+            }
+        );
+    });
+}
+
+async function writeToFile(data) {
+    return new Promise((res, rej) => {
+        fs.appendFile(config.types_file, data, (err) => {
+            if (err) rej(err);
+            else res();
+        });
     });
 }
